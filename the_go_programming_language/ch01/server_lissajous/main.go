a3b9fff8087b34391b080fc834bcdf5ecd9f4e18
reign <reignofmiracle@gmail.com>
2020-04-22 19:01:07 +0900

    working on gopath

---
+++ the_go_programming_language/ch01/server_lissajous/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		Lissajous(w)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
