103a39aee1a07f04c254363b8c1e48e4de0ce9bd
reign <reignofmiracle@gmail.com>
2020-04-29 14:19:18 +0900

    working on charcount

---
+++ the_go_programming_language/ch04/charcount/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.Newreader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+}
