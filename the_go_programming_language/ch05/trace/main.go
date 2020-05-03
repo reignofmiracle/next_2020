20e95c6a0060d84a700b30ecf535a7b1fe7d58df
reign <reignofmiracle@gmail.com>
2020-05-04 03:11:15 +0900

    update ch05

---
+++ the_go_programming_language/ch05/trace/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	testfunc()
+}
+
+func testfunc() {
+	defer trace("test")()
+	time.Sleep(10 * time.Second)
+}
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() { log.Printf("exit %s (%s)", msg, time.Since(start)) }
+}
