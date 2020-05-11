53708f1d81910713fe227ae30514893b63355e96
reign <reign@reignui-MacBookPro.local>
2020-05-11 23:07:05 +0900

    working on ch07

--- the_go_programming_language/ch07/eval/eval.go
+++ the_go_programming_language/ch07/eval/eval.go
@@ -3,10 +3,12 @@ package eval
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 type Expr interface {
 	Eval(env Env) float64
+	Check(vars map[Var]bool) error
 }
 
 type Var string
@@ -73,3 +75,51 @@ func (c call) Eval(env Env) float64 {
 	}
 	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
 }
+
+// Check ..
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
+func (literal) Check(vars map[Var]bool) error {
+	return nil
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
+}
+
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("unexpected binary op %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	if err := b.y.Check(vars); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d", c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
