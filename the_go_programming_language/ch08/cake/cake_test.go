0adda1442ce77426971023bd318d8c8bb32e2bdc
reign <reignofmiracle@gmail.com>
2020-05-14 17:29:03 +0900

    working on ch08

---
+++ the_go_programming_language/ch08/cake/cake_test.go
@@ -0,0 +1,48 @@
+package cake
+
+import (
+	"testing"
+	"time"
+)
+
+var defaults = Shop{
+	Verbose:      false,
+	Cakes:        20,
+	BakeTime:     10 * time.Millisecond,
+	NumIcers:     1,
+	IceTime:      10 * time.Microsecond,
+	InscribeTime: 10 * time.Millisecond,
+}
+
+func Benchmark(b *testing.B) {
+	cakeshop := defaults
+	cakeshop.Work(b.N)
+}
+
+func BenchmarkBuffers(b *testing.B) {
+	cakeshop := defaults
+	cakeshop.BakeBuf = 10
+	cakeshop.IceBuf = 10
+	cakeshop.Work(b.N)
+}
+
+func BenchmarkVariable(b *testing.B) {
+	cakeshop := defaults
+	cakeshop.BakeStdDev = cakeshop.BakeTime / 4
+	cakeshop.IceStdDev = cakeshop.IceTime / 4
+	cakeshop.InscribeStdDev = cakeshop.InscribeTime / 4
+	cakeshop.Work(b.N)
+}
+
+func BenchmarkSlowIcing(b *testing.B) {
+	cakeshop := defaults
+	cakeshop.IceTime = 50 * time.Millisecond
+	cakeshop.Work(b.N)
+}
+
+func BenchmarkSlowIcingManyIcers(b *testing.B) {
+	cakeshop := defaults
+	cakeshop.IceTime = 50 * time.Millisecond
+	cakeshop.NumIcers = 5
+	cakeshop.Work(b.N)
+}
