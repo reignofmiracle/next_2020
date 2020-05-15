1e6722f2c3dd5040ac7a40bca95bb7d06d5da48d
reign <reign@reignui-MacBookPro.local>
2020-05-16 00:50:43 +0900

    update ch08

---
+++ the_go_programming_language/ch08/crawl3/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"../links"
+)
+
+func main() {
+	worklist := make(chan []string)
+	unseenLinks := make(chan string)
+
+	go func() { worklist <- []string{"http://gopl.io/"} }()
+
+	for i := 0; i < 20; i++ {
+		go func() {
+			for link := range unseenLinks {
+				foundLinks := crawl(link)
+				go func() { worklist <- foundLinks }()
+			}
+		}()
+	}
+
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				unseenLinks <- link
+			}
+		}
+	}
+}
+
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{}
+	list, err := links.Extract(url)
+	<-tokens
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
